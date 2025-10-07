1aee7be03fcdb264d35680d9303f84ffad5cd374
LAKSHMI PRABHA V <130069907+Prabha2005@users.noreply.github.com>
2025-10-07 18:10:40 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,17 @@
 
 <?php
-// Final verified database connection file
+// database connection file
 
 $servername = "localhost";
 $username   = "root";           // same as Workbench user
-$password   = "sqlvlp@1";               // add your MySQL password here if you set one
+$password   = " ";               // add your password here if you set one (I don't want to show my password here)
 $database   = "userdb";
-$port       = 3306;             // confirmed working port
+$port       = 3306;             // working port
 
 $conn = new mysqli($servername, $username, $password, $database, $port);
 
 // Check connection
 if ($conn->connect_error) {
-    die("❌ Database connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
